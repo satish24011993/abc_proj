28092e31cdc793edcae25c1fb0cd668b0e69a80d
satish24011993 <satishkurakula073@gmail.com>
2021-04-16 17:04:42 +0530

    Deployment

--- task_project/settings.py
+++ task_project/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT= os.path.join(BASE_DIR,'/static/')
 
-STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFileStorage'
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
 prod_db=dj_database_url.config(conn_max_age=500)
